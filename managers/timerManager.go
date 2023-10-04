f39dabfe520e5500af54bb83bfaaad890608ccbb
Hugo J. Bello <hjbello.wk@protonmail.com>
2023-10-04 14:11:01 +0200

    clean

--- managers/timerManager.go
+++ managers/timerManager.go
@@ -12,7 +12,7 @@ type TimerManager struct {
 
 func (m *TimerManager) SetTimer(app *tview.Application, globalAppState *models.GlobalAppState) {
 
-	for now := range time.Tick(60 * time.Second) {
+	for now := range time.Tick(5 * 60 * time.Second) {
 		if globalAppState.RefreshBlocked == false {
 			app.Stop()
 			*&globalAppState.CurrentTime = now
